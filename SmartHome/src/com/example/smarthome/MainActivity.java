eb82a4517588aa8995fc4c35517c22fb6c008991
sebastian.mueller <sebastian.mueller@192.168.1.106>
2012-12-18 23:50:59 +0100

    fragment architecture established

--- SmartHome/src/com/example/smarthome/MainActivity.java
+++ SmartHome/src/com/example/smarthome/MainActivity.java
@@ -2,9 +2,15 @@ package com.example.smarthome;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
+	
+	/* request BT enable */
+	private static final int REQUEST_ENABLE_BT	= 0x1;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -13,10 +19,48 @@ public class MainActivity extends Activity {
 	}
 
 	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+
+	    /*
+	     * Force the system to close the application down completely instead of
+	     * retaining it in the background. The process that runs the application
+	     * will be killed. The application will be completely created as a new
+	     * application in a new process if the user starts the application
+	     * again.
+	     */
+	    System.exit(0);
+	}
+
+	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
+	}
+	
+	public void closeApp(View view){
+		this.onDestroy();
+	}
+	
+	public void activateBT(View view){
+		
+		/**
+		 * Checks if Bluetooth is supported by own device
+		 */
+		BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+		if (mBluetoothAdapter == null) {
+		    // Device does not support Bluetooth
+		}
+		
+		if (!mBluetoothAdapter.isEnabled()) {
+		    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+		    startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+		}
+	}
+	
+	public void discoverBT(View view){
+		//this.onDestroy();
 	}
 
 }
